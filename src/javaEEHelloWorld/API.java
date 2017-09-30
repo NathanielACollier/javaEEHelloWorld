31e7a1d004bb0f8920986e38b801fbdf671ffb3e
Nathaniel Collier <nathaniel.collier@aecc.com>
2017-09-30 17:24:08 -0500

    Put in some stuff to send back hello world on the servlet

--- src/javaEEHelloWorld/API.java
+++ src/javaEEHelloWorld/API.java
@@ -26,7 +26,8 @@ public class API extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String userName = request.getRemoteUser();
+		response.getWriter().write("Hello, "+userName+"!");
 	}
 
 	/**
